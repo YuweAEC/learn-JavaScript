2f5fc9a945922f3d2c725940ebddeef75c1719d9
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-01 08:04:10 +0530

    feat: add new codes

--- basics/function.js
+++ basics/function.js
@@ -1,18 +1,18 @@
-// function start() {
-//     console.log("This is a function"); // statement 1
-//   }
+function start() {
+    console.log("This is a function"); // statement 1
+  }
   
-//   start;
+  start;
   
-//   start(); //--> this runs line 1 in func Body, i.e statement 1
-//   // o/p->This is a function
+  start(); //--> this runs line 1 in func Body, i.e statement 1
+  // o/p->This is a function
   
-//   function stop() {
-//     const x = 10; // statement 1
-//     return x; // statement 2
-//   }
+  function stop() {
+    const x = 10; // statement 1
+    return x; // statement 2
+  }
   
-//   stop();
+  stop();
 
 let x = myFunction(4, 3);
 
@@ -21,8 +21,14 @@ function myFunction(a, b) {
 }
 
 
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// }
+
+// let value = toCelsius(77);
+
 function toCelsius(fahrenheit) {
   return (5/9) * (fahrenheit-32);
 }
 
-let value = toCelsius(77);
+let value = toCelsius();
