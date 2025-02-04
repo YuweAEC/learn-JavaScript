608087739c10d4641ce579d266591e605dae7b7c
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-04 06:48:31 +0530

    feat: update

---
+++ basics/operations.js
@@ -0,0 +1,8 @@
+// to do : use w3school as a preference to practice operators 
+
+let a = 10, b = 5;
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a % b);
