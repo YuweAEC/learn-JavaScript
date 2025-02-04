1c806328b8bf721f07597362cd455b2d3763d43c
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-04 06:54:26 +0530

    feat: update

---
+++ basics/const.js
@@ -0,0 +1,12 @@
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
