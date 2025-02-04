b65f5444198d7cd0d35ff8ee32e2663f448a2155
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-04 06:49:05 +0530

    feat: create new file

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+let str = "Hello";
+let num = 100; 
+
+let bool = true; 
+let obj = { name: "Alice" }; 
+let arr = [1, 2, 3]; 
+let undef; 
+let nul = null; 
+
+console.log(typeof str, typeof num, typeof bool);
---
+++ basics/ifelse.js
@@ -0,0 +1,8 @@
+let score = 80;
+if (score >= 90) {
+    console.log("Grade: A");
+} else if (score >= 75) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
