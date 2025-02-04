8d5dc4af4e33fcecd3602a8fddadd32eec37729a
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-04 06:58:45 +0530

    feat: update new codes

--- basics/ifelse.js
+++ basics/ifelse.js
@@ -1,3 +1,24 @@
+let x;
+
+// ---------- if , else, else if -----  TODO //
+
+// if (x) {
+//   console.log("x is defined");
+// } else {
+//   console.log("x is not defined");
+// }
+
+if (x) {
+  console.log("x is defined");
+} else {
+  console.log("x is not defined");
+  //   return;
+}
+if (x === undefined) {
+  console.log("x is undefined");
+}
+
+
 let score = 80;
 if (score >= 90) {
     console.log("Grade: A");
--- basics/operations.js
+++ basics/operations.js
@@ -1,8 +1,60 @@
 // to do : use w3school as a preference to practice operators 
+const x = 10;
+const y = x + 20;
+let z = x + y;
 
-let a = 10, b = 5;
-console.log(a + b); 
-console.log(a - b); 
-console.log(a * b); 
-console.log(a / b); 
-console.log(a % b);
+// ----- Shorthand notation -----
+
+z += 10; // z=z+10
+
+z /= 10; // z=z/10
+
+z %= 3; // z=z%3
+
+z *= 2; // z=z*2
+
+// ----- Comparison Operators -----
+// Output all of the comarision is always boolean
+// O/p: true/false, x=10, y=30, z=4
+
+console.log(x == y);
+console.log(x === y);
+x != y;
+z !== y;
+z > y;
+x < y;
+undefined >= y;
+null <= y;
+
+// ----- (== vs ===) OR (!= vs !==) -----
+const a = "10";
+const b = 10;
+console.log(a == b);
+console.log(a === b);
+
+console.log(a != b);
+console.log(a !== b);
+
+// -------- Ternary Operator --------- TODO
+// (condition) ? true : false
+const t = x === y && z === 4 ? "hello" : "world";
+
+if (x === y && z === 4) {
+    return "hello";
+} else {
+    return "world";
+}
+
+// ----- Logical Operators ----- TODO
+
+// ----- Bitwise Operators ----- TODO
+
+// ----- String Concatenation(Addition) ----- TODO
+// e.g "Hello" + "World";
+
+// let a = 10, b = 5;
+// console.log(a + b); 
+// console.log(a - b); 
+// console.log(a * b); 
+// console.log(a / b); 
+// console.log(a % b);
