96909a1f1aed4e38767cdeb838d0181ca596ceee
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-05 06:42:23 +0530

    update

--- basics/objwithspread.js
+++ basics/objwithspread.js
@@ -4,16 +4,16 @@ const dog = { ...species, breed: "Labrador", color: "Golden" };
 const cat = { ...species, breed: "Siamese", color: "White" };
 
 const dog1 = {
-  type: "Mammal",
-  movement: "Walks",
-  breed: "Labrador",
-  color: "Golden",
+    type: "Mammal",
+    movement: "Walks",
+    breed: "Labrador",
+    color: "Golden",
 };
 const cat1 = {
-  type: "Mammal",
-  movement: "Walks",
-  breed: "Siamese",
-  color: "White",
+    type: "Mammal",
+    movement: "Walks",
+    breed: "Siamese",
+    color: "White",
 };
 
 // TODO Practice more
