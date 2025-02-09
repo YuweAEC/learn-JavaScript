aff30e13f8715cbc063a377721b379c34701a7d9
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-09 07:09:43 +0530

    refactor: export named in one line

--- basics/commonexport.js
+++ basics/commonexport.js
@@ -1,10 +1,10 @@
-export const name = "Yuvraj Singh";
-export function morning() {
+const name = "Yuvraj Singh";
+function morning() {
     return "Hello, welcome to JS modules!";
 }
-
 
 export default function add(a, b) {
     return a + b;
 }
 
+export { name, morning };
