513319a5c46323d9017eaba46ba67cfed76d4ce9
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-18 22:56:22 +0530

    update

---
+++ basics/let.js
@@ -0,0 +1,30 @@
+{
+    let x = 2;
+}
+  // x can NOT be used here
+
+{
+    var x = 2;
+}
+  // x CAN be used here
+
+let x = "John Doe";
+
+let x = 0;
+
+var x = "John Doe";
+
+var x = 0;
+
+var x = 10;
+// Here x is 10
+
+{
+var x = 2;
+// Here x is 2
+}
+
+// Here x is 2
+
+
+
